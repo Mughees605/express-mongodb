fb9cd3cd82472095fe2e7a6bc7c456bd986142b2
mughees605 <mughees605@gmail.com>
2017-07-19 12:47:24 +0500

    modified

--- index.js
+++ index.js
@@ -1,18 +1,76 @@
-import http from 'http';
-import router from "./src/routes";
-import express from "express"
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 
-const app = express();
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use('/api',router)
-// http.createServer((req, res)=> {
-//     res.writeHead(200,{'Content-type':'text/plain'});
-//     res.end('HELLO WORLD');
-// }).listen(3000,'localhost');
-mongoose.connect('mongodb://mughees605:mughees1997@ds163701.mlab.com:63701/es6-mongoose')
-app.listen(process.env.PORT || 4000, function () {
-    console.log("4000")
+
+var app = express();
+var port = (process.env.PORT || 4000);
+
+app.use(bodyParser.json());
+
+var studentSchema = new mongoose.Schema({
+    name : String,
+    rollNum:Number,
+    class:String
+})
+
+var studentModel = mongoose.model("student",studentSchema);
+
+app.post("/post",function(req,res,next){
+
+    var newStudent = new studentModel({
+        name : req.body.name,
+    })
+    newStudent.save(function(err,data){
+
+        if(!err){
+            console.log("student is saved");
+            res.send("student is saved");
+        }
+        else{
+            res.send("student saving failed");
+            console.log("student savig failed");
+        }
+
+    })
+})
+
+app.get('/post', function (req, res) {
+    
+        studentModel.find({}, function (err, docs) {
+            res.json(docs);
+            console.log(docs);
+        });
+    
+});
+
+
+
+// app.get("/",function(req,res,next){
+
+//    // res.send("hello world")
+//    studentModel.find(function(err,doc){
+//    if(err){
+//        res.send(err);
+//    }
+//    else{
+//        res.send(doc);
+//    }
+//    })
+       
+app.get("/home",function(req,res,next){
+
+    res.send("home");
+
+
+});
+
+app.listen(port,function(){
+    console.log("local host is on port 3000");
+})
+
+
+mongoose.connect('mongodb://mughees605:mughees1996@ds151289.mlab.com:51289/hello-world');
+mongoose.connection.on("connected",function(){
+    console.log("moongose is connected")
 })

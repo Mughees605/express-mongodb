dacfc704c869d5403da33cf2b5c2c6e80f83e60a
mughees605 <mughees605@gmail.com>
2017-07-19 18:57:48 +0500

    added

--- server.js
+++ server.js
@@ -9,10 +9,8 @@ const port = process.env.PORT || 4000;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api',router)
-// http.createServer((req, res)=> {
-//     res.writeHead(200,{'Content-type':'text/plain'});
-//     res.end('HELLO WORLD');
-// }).listen(3000,'localhost');
+
+
 mongoose.connect('mongodb://mughees605:mughees1997@ds163701.mlab.com:63701/es6-mongoose')
 app.listen(port,() => {
     console.log("server is running at : " + process.env.PORT || 8080)

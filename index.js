fa2ce568cbc6faf2efd3a656173953d521930593
mughees605 <mughees605@gmail.com>
2017-07-19 13:02:24 +0500

    port added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import router from "./src/routes";
 import express from "express"
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-
+const port = process.env.PORT || 4000;
 const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -13,6 +13,6 @@ app.use('/api',router)
 //     res.end('HELLO WORLD');
 // }).listen(3000,'localhost');
 mongoose.connect('mongodb://mughees605:mughees1997@ds163701.mlab.com:63701/es6-mongoose')
-app.listen(process.env.PORT || 4000, function () {
-    console.log("4000")
+app.listen(port, function () {
+    console.log("server is running at : " + port)
 })

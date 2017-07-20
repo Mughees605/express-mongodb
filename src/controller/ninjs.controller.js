5c1c078bcccdce2b8e383d687e45b4729b31a853
mughees605 <mughees605@gmail.com>
2017-07-20 13:29:52 +0500

    changing controller names

--- src/controller/ninjs.controller.js
+++ src/controller/ninjs.controller.js
@@ -1,17 +1,17 @@
 import NinjaModel from '../models/ninja.model';
 
-export class Ninja {
-    
+export default class Ninja {
+
     constructor() { }
 
-    static getNinja(req, res) {
+    static get(req, res) {
         NinjaModel.find({}, function (err, user) {
             if (err) throw err;
             res.send({ status: true, data: user });
         })
     }
 
-    static postNinja(req, res) {
+    static post(req, res) {
         console.log('req.body ', req.body);
         const newNinja = new NinjaModel(req.body);
         newNinja.save(function (err, record) {
@@ -23,7 +23,7 @@ export class Ninja {
         })
     }
 
-    static putNinja(req, res, next) {
+    static put(req, res, next) {
         var id = req.params.id;
         NinjaModel.findByIdAndUpdate({ _id: req.params.id }, req.body).then(() => {
             NinjaModel.findOne({ _id: id }).then((ninja) => {
@@ -32,7 +32,7 @@ export class Ninja {
         }).catch(next)
     }
 
-    static deleteNinja(req, res, next) {
+    static delete(req, res, next) {
         NinjaModel.findByIdAndRemove({ _id: req.params.id }).then((ninja) => {
             console.log(ninja);
             res.send(ninja)
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 import { Router } from 'express';
-import { Ninja } from './controller/ninjs.controller'
+import Ninja from './controller/ninjs.controller'
 import { postNinja, putNinja, deleteNinja } from "./controller/ninjs.controller"
 
 const router = Router();
 
-router.get('/',Ninja.getNinja);
+router.get('/',Ninja.get);
 
-router.post('/add',Ninja.postNinja);
+router.post('/add',Ninja.post);
 
-router.delete('/delete/:id',Ninja.deleteNinja);
+router.delete('/delete/:id',Ninja.delete);
 
-router.put('/add/:id',Ninja.putNinja);
+router.put('/add/:id',Ninja.put);
 
 
 export default router;

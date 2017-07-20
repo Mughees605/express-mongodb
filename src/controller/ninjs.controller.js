f75edd9bbd03f360ebc3c42b273bffba0bc576f3
mughees605 <mughees605@gmail.com>
2017-07-20 13:00:10 +0500

    creating a put and delete routes

--- src/controller/ninjs.controller.js
+++ src/controller/ninjs.controller.js
@@ -1,14 +1,14 @@
 import Ninja from '../models/ninja.model';
 
 export let getNinja = (req, res) => {
-    Ninja.find({},function(err,user){
-      if(err) throw err;
-     res.send({ status: true, data: user });
+    Ninja.find({}, function (err, user) {
+        if (err) throw err;
+        res.send({ status: true, data: user });
     })
 }
 
 export let postNinja = (req, res) => {
-    console.log('req.body ',req.body);
+    console.log('req.body ', req.body);
     const newNinja = new Ninja(req.body);
     newNinja.save(function (err, record) {
         if (err) {
@@ -19,8 +19,20 @@ export let postNinja = (req, res) => {
     })
 }
 
-export let putNinja = (req, res) => {
-    console.log('req.body ',req.body);
-    var myParams = req.params.id;
-    
+export let putNinja = (req, res, next) => {
+    console.log('req.body ', req.body);
+    var id = req.params.id;
+    Ninja.findByIdAndUpdate({ _id: id }, req.body).then(() => {
+        Ninja.findOne({ _id: id }).then((ninja) => {
+            res.send(ninja)
+        });
+    }).catch(next)
 }
+
+export let deleteNinja = (req, res, next) => {
+    Ninja.findByIdAndRemove({ _id: req.params.id }).then((ninja) => {
+        console.log(ninja);
+        res.send(ninja)
+    })
+
+} 
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
-import { getNinja, postNinja } from "./controller/ninjs.controller"
+import { getNinja, postNinja, putNinja, deleteNinja } from "./controller/ninjs.controller"
 
 const router = Router();
 
 router.get('/',getNinja)
 
 router.post('/add',postNinja)
-// router.put('/add/:id',putNinja)
+
+router.delete('/delete/:id',deleteNinja)
+
+router.put('/add/:id',putNinja)
 
 
 export default router;
